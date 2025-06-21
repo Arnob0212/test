06a5ca6c12e6825ce0c4c0b600c95eb94aed0d29
Arnob0212 <chakrabortyarnob788@gmail.com>
2025-06-21 13:34:36 +0530

    Add files via upload

---
+++ 6361882_Week1/6361882_Week2_Algorithms_Data_Structures/Exercise2_E-Commerce/Exercise7_Financial_Forcasting/Forcasting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance_Forcasting
+{
+    public class Forecast
+    {
+        // Simple recursive method to calculate future value
+        public double GetFutureValue(double value, double rate, int year)
+        {
+            if (year == 0)
+            {
+                return value;
+            }
+
+            double newValue = value + (value * rate);
+            return GetFutureValue(newValue, rate, year - 1);
+        }
+    }
+}
