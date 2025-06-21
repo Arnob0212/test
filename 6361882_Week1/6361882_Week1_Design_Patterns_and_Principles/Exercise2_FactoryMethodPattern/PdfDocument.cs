f5722a9bcde2d384472d171fbee1302426931794
Arnob0212 <chakrabortyarnob788@gmail.com>
2025-06-21 12:42:45 +0530

    Add files via upload

---
+++ 6361882_Week1/6361882_Week1_Design_Patterns_and_Principles/Exercise2_FactoryMethodPattern/PdfDocument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPatternExample
+{
+    public class PdfDocument : Document
+    {
+        public void Open()
+        {
+            Console.WriteLine("PDF document is opened.");
+        }
+    }
+}
---
+++ 6361882_Week1/6361882_Week1_Design_Patterns_and_Principles/Exercise2_FactoryMethodPattern/WordDocument.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodPatternExample
+{
+    public class WordDocument : Document
+    {
+        public void Open()
+        {
+            Console.WriteLine("Word document is opened.");
+        }
+    }
+}
