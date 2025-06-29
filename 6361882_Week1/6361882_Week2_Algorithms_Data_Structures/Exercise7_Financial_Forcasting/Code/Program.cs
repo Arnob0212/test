e0efe04edeb7a5a37f0bdb7add7915b86774cfd0
Arnob0212 <chakrabortyarnob788@gmail.com>
2025-06-29 16:54:13 +0530

    Add files via upload

---
+++ 6361882_Week1/6361882_Week2_Algorithms_Data_Structures/Exercise7_Financial_Forcasting/Code/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance_Forcasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forecast f = new Forecast();
+
+            Console.WriteLine("Enter current value:");
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter yearly growth rate (in %):");
+            double rate = Convert.ToDouble(Console.ReadLine()) / 100;
+
+            Console.WriteLine("Enter number of years:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            double result = f.GetFutureValue(amount, rate, n);
+
+            Console.WriteLine("Future value is: " + result);
+        }
+    }
+}
+
